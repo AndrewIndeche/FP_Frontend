4f153521cecc8319dfe71fbfdea36da7b8f59328
Vitalis-Kirui <vitaliskirui1@gmail.com>
2021-11-23 11:51:34 +0300

    updating reports component

--- src/app/payroll-new-run/payroll-new-run.component.ts
+++ src/app/payroll-new-run/payroll-new-run.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PayrollService } from '../payroll.service';
+import { StaffProcessService } from '../staff-processes.service'
 @Component({
   selector: 'app-payroll-new-run',
   templateUrl: './payroll-new-run.component.html',
@@ -8,14 +9,25 @@ import { PayrollService } from '../payroll.service';
 })
 export class PayrollNewRunComponent implements OnInit {
   payrolls: any;
+  staffs: any;
+
   getName() {
     this.payroll.getdetails().subscribe((data: any) => {
       this.payrolls = data;
       console.log(this.payrolls);
     });
   }
-  constructor(private payroll: PayrollService, private http: HttpClient) { }
+
+  getStaff() {
+    this.staff.getdetails().subscribe((data: any) => {
+      this.staffs = data;
+      console.log(this.staffs);
+    });
+  }
+
+  constructor(private payroll: PayrollService, private staff: StaffProcessService, private http: HttpClient) { }
   ngOnInit(): void {
     this.getName()
+    this.getStaff()
   }
 }
