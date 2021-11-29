43bd24eb46d44b1be556951a0cc2d0fb8bd1a2a3
AndrewIndeche <indecheandrew@gmail.com>
2021-11-29 12:22:41 +0300

    change staff overview template

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../services/api.service';
+import { Staff } from '../classes/staff';
 
 @Component({
   selector: 'app-overview',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview.component.css']
 })
 export class OverviewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  staff:any
+  constructor(private api:ApiService) { }
+  lststaff: Staff[] = [];
+  ngOnInit(){
+    this.api.getstaff().subscribe(
+      data=>{
+        this.lststaff=data;
+      }
+    )
   }
-
 }
