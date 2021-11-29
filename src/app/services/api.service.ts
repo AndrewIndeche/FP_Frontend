60d8857360617d65a322705ed40b63fc680ce3bb
michael otieno <michael.otieno@student.moringaschool.com>
2021-11-29 21:50:09 +0300

    navbar changes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,7 +7,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiService {
   constructor(private httpclient:HttpClient){}
-    getstaff():Observable<any>{
-        return this.httpclient.get('https://financefuzu.herokuapp.com/finance/api/staffw/')
-    }
+
+
+  getstaff():Observable<any>{
+    return this.httpclient.get('https://financefuzu.herokuapp.com/finance/api/staffw/')
+  }
+
+  getEmployee(id:number):Observable<Object>{
+    return this.httpclient.get('http://127.0.0.1:8000/api/staff/'+`${id}/`);
+  }
+    
   }
